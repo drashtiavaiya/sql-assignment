f524c599858400f3bbe8b800a1d6c31ea5b6b7e1
drashtiavaiya <8zvgmj527t@privaterelay.appleid.com>
2026-09-18 09:56:08 -0700

    session 2 sql queries and screenshots

---
+++ session2_basic_select.sql
@@ -0,0 +1,5 @@
+SELECT * from restaurants;
+SELECT name, rating FROM zomato_reviews;
+SELECT movie_name AS Title, release_year AS 'Year Released' FROM movies;
+-- This query selects all columns from the products table
+SELECT * FROM products;
